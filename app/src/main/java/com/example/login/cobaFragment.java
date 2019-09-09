3ac05d5fc513da3f2498f540e5a70816f6f9697f
Nicholas Christianto <nicholas.christianto@ti.ukdw.ac.id>
2019-09-09 19:08:58 +0700

    fragment

---
+++ app/src/main/java/com/example/login/cobaFragment.java
@@ -0,0 +1,15 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cobaFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coba_fragment);
+    }
+
+}
